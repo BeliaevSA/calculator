9295fbceecbc5b4a2a198a57013a023243021f7f
Beliaev Sergei <beliaevsa1@gmail.com>
2022-09-22 18:32:55 +0300

    editing styles

--- src/components/Text.js
+++ src/components/Text.js
@@ -2,7 +2,9 @@ const Text = (props) => {
 
   const value = props.text.join('') === '0' ? props.values.num1 : props.text.join('')
 
-  const finishValue = value.length > 9 ? String((+value).toExponential(4)) : value
+  let finishValue = value.length > 9 ? String((+value).toExponential(4)) : value
+  
+  if(finishValue === 'Infinity') finishValue = 'Ошибка'
 
   const addingSpace = (str) => {
     // if(str.includes('е')) return
@@ -13,10 +15,10 @@ const Text = (props) => {
     }
     return arr.reverse().join('')
   }
-  console.log(finishValue.split(''))
+  console.log(typeof finishValue)
   return (
     // <p className='text' >{value}</p>
-    <p className='text' >{(finishValue.split('').includes('.') || finishValue.split('').includes('е') ) ? finishValue : addingSpace(finishValue)}</p>
+    <p className='text' >{(finishValue.split('').includes('.') || finishValue.split('').includes('е') || finishValue === 'Ошибка' ) ? finishValue : addingSpace(finishValue)}</p>
     
   )
 }
